b1fd849d7f53f9ddaed51a6c2544806ba727ac1f
cmccandless <cmccandless@users.noreply.github.com>
2016-04-14 20:06:15 -0500

    Changes to remoteDorm.py

--- remoteDorm.py
+++ remoteDorm.py
@@ -4,5 +4,4 @@ import serial
 import time
 ser = serial.Serial('/dev/rfcomm0', 9600, timeout=0)
 while True:
-    var = str(input("Enter something: "))
-    ser.write(var.encode())
+    ser.write(str(input("Enter something: ")).encode())
